80c420bf99f3260a8789ed21ca25c6dfb04090dd
Steven Van Ingelgem <steven@vaningelgem.be>
2020-08-13 08:22:37 +0200

    Pathlike is only known in 3.6+

--- build.py
+++ build.py
@@ -44,7 +44,6 @@ def set_properties(project):
             'Intended Audience :: Developers',
             'Topic :: Software Development :: Libraries :: Python Modules',
             'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
